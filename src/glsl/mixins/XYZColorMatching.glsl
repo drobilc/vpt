acedfd8d9501c1098f09ecfc65fb9927ca40439c
Niki Bizjak <drobilc@gmail.com>
2022-05-16 10:39:16 +0200

    Fix XYZ color matching function implementation

--- src/glsl/mixins/XYZColorMatching.glsl
+++ src/glsl/mixins/XYZColorMatching.glsl
@@ -4,6 +4,10 @@
 // Approximations to the CIE XYZ Color Matching Functions".
 // https://jcgt.org/published/0002/02/01/
 
+const uint numberOfSamples = 32u;
+const float startWavelength = 400.0;
+const float endWavelength = 700.0;
+
 float S(float x, float y, float z) {
     return x < 0.0 ? y : z;
 }
@@ -15,7 +19,7 @@ float X(float x) {
     float v2 = dx2 * S(dx2, 0.0264, 0.0323);
     float dx3 = x - 501.1;
     float v3 = dx3 * S(dx3, 0.0490, 0.0382);
-    return 0.362 * exp(0.5 * v1 * v1) + 1.056 * exp(0.5 * v2 * v2) - 0.065 * exp(0.5 * v3 * v3);
+    return 0.362 * exp(-0.5 * v1 * v1) + 1.056 * exp(-0.5 * v2 * v2) - 0.065 * exp(-0.5 * v3 * v3);
 }
 
 float Y(float x) {
@@ -23,7 +27,7 @@ float Y(float x) {
     float v1 = dx1 * S(dx1, 0.0213, 0.0247);
     float dx2 = x - 530.9;
     float v2 = dx2 * S(dx2, 0.0613, 0.0322);
-    return 0.821 * exp(0.5 * v1 * v1) + 0.286 * exp(0.5 * v2 * v2);
+    return 0.821 * exp(-0.5 * v1 * v1) + 0.286 * exp(-0.5 * v2 * v2);
 }
 
 float Z(float x) {
@@ -31,23 +35,23 @@ float Z(float x) {
     float v1 = dx1 * S(dx1, 0.0845, 0.0278);
     float dx2 = x - 459.0;
     float v2 = dx2 * S(dx2, 0.0385, 0.0725);
-    return 1.217 * exp(0.5 * v1 * v1) + 0.681 * exp(0.5 * v2 * v2);
+    return 1.217 * exp(-0.5 * v1 * v1) + 0.681 * exp(-0.5 * v2 * v2);
 }
 
 vec3 xyzResponseAt(float x) {
     return vec3(X(x), Y(x), Z(x));
 }
 
-vec3 averageXYZResponseInBin(float fromLambda, float toLambda, uint numberOfSamples) {
+vec3 averageXYZResponseInBin(float fromLambda, float toLambda, uint subsamples) {
     vec3 result = vec3(0, 0, 0);
 
     float interval = toLambda - fromLambda;
-    float sectionWidth = interval / float(numberOfSamples);
+    float sectionWidth = interval / float(subsamples);
 
-    for (uint i = 0u; i < numberOfSamples; i++) {
+    for (uint i = 0u; i < subsamples; i++) {
         float lambda = fromLambda + float(i) * sectionWidth;
         result += xyzResponseAt(lambda);
     }
 
-    return result / float(numberOfSamples);
+    return result / float(subsamples);
 }
